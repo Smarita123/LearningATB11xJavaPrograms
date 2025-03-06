34e46121dbe0a445194ccfe525549fa092ac3a5f
Smarita123 <smaritad@gmail.com>
2025-03-06 08:23:23 +0530

    first commit

---
+++ src/ex_01_Java_Basic/HelloWorld.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
